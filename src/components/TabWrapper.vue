4f8ae1c633645b32430a9e7d146f4f22e9f8b8e9
ugurcanatas <ugurcanemre@hotmail.com>
2021-03-06 22:52:45 +0300

    kou - web scraping keyword frequency

---
+++ src/components/TabWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TabWrapper"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/childs/URLFrekans.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "URLFrekans"
+}
+</script>
+
+<style scoped>
+
+</style>
